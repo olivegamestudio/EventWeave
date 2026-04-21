e2aed5f7c8a29f7deaa3755d378b02347e90f72d
Simon Jefferies <simon.jefferies@inseinc.com>
2026-04-21 18:39:32 +0100

    Add EventWeave pub/sub library with CommunityToolkit adapter
    Introduces the EventWeave solution with two packages:
    - `EventWeave`: lightweight event aggregator using weak
      references for zero-dependency pub/sub messaging
    - `EventWeave.CommunityToolkit`: adapter bridging
      `IEventAggregator` with CommunityToolkit.Mvvm `IMessenger`

---
+++ src/EventWeave.CommunityToolkit/MessengerEventAggregator.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System.Runtime.CompilerServices;
+
+namespace EventWeave;
+
+/// <summary>
+/// Infrastructure implementation of the <see cref="IEventAggregator"/>, wrapping the <see cref="IMessenger"/> from the Community Toolkit.
+/// </summary>
+/// <param name="messenger">The underlying messenger used for message dispatch.</param>
+public sealed class MessengerEventAggregator(IMessenger messenger) : IEventAggregator
+{
+	const int _token = 0;
+
+	/// <inheritdoc />
+	public void Subscribe<TMessage>(object service, Action<TMessage> action) where TMessage : class
+	{
+		messenger.Register<object, TMessage, int>(service, _token, (_, message) => action(message));
+	}
+
+	/// <inheritdoc />
+	public void Unsubscribe<TMessage>(object subscriber) where TMessage : class
+	{
+		messenger.Unregister<TMessage, int>(subscriber, _token);
+	}
+
+	/// <inheritdoc />
+	public void Publish<TMessage>(TMessage message, [CallerMemberName] string? caller = null, [CallerFilePath] string? file = null) where TMessage : class
+	{
+		messenger.Send(message, _token);
+	}
+}
